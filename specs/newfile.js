abea880d883c2e2853ec262f1ba2bc283aa284ca
yulia.konovalchyk@gmail.com <yulia.konovalchyk@gmail.com>
2024-02-15 17:37:24 +0300

    Add new file

--- specs/newfile.js
+++ specs/newfile.js
@@ -1,1 +1,25 @@
 //test
+describe('wild', () => {
+
+    before(async()=>{
+      //await browser.url('https://www.wildberries.by/');
+      await homePage.open();
+      
+
+    });
+
+    it('Test Case Description', async () => {
+      await expect(browser).toHaveUrl('https://www.wildberries.by/');
+    });
+    it('wild', async () => {
+
+  //await HomePage.open();
+       // await browser.maximizeWindow();
+        const text1 = await $('a[data-text="strBecomeASeller"]');
+        //await browser.debug()
+        
+       await expect (text1).toHaveText('Стать продавцом');
+
+    }
+    );
+  });
